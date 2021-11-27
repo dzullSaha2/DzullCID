e17f556cf02cf8cd3fa762f879c05e8f56ec8eb2
dzullSaha2 <93919864+dzullSaha2@users.noreply.github.com>
2021-11-27 19:52:21 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,13 @@
 sudo apt-get install clang
-clang++ CPPBot.cpp enet/callbacks.c enet/compress.c enet/host.c enet/list.c enet/packet.c enet/peer.c enet/protocol.c enet/unix.c -pthread -std=c++11 -fpermissive -o createid -w #Build source code into executable
 sudo apt-get install boxes
-echo -e "Tool succesfuly builded, you can start creating ID by typing ./createid \n\tEdited by github.com/CapciGithub"| boxes -d parchment -a c
+clang++ CPPBot.cpp enet/callbacks.c enet/compress.c enet/host.c enet/list.c enet/packet.c enet/peer.c enet/protocol.c enet/unix.c -pthread -std=c++11 -fpermissive -o DzullCID -w #Build source code into executable
+echo ""
+echo "================================="
+echo "⚠This Script Remaked By Dzull#8839⚠"
+echo ""
+echo "Tool CID Successfully Builded,"
+echo ""
+echo "Kamu bisa ketik ./DzullCID sekarang!!!"
+echo ""
+echo "|Remaked By Dzull|"
+echo "================================="
